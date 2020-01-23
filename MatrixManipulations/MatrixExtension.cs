4155bfe7e05ab6590ce35e188dfc13372ceaf9db
andrjak <79821843rt@gmail.com>
2020-01-23 10:49:02 +0300

    implement GetMatrix method

--- MatrixManipulations/MatrixExtension.cs
+++ MatrixManipulations/MatrixExtension.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace MatrixManipulations
 {
@@ -25,9 +26,63 @@ namespace MatrixManipulations
         ///     11 16 15 6
         ///     10 9  8  7.
         /// </example>
+        #pragma warning disable CA1814 // By condition, a multidimensional array is required
         public static int[,] GetMatrix(int size)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (size <= 0)
+            {
+                throw new ArgumentException($"Size of matrix '{size}' cannot be less or equal zero.");
+            }
+
+            if (size > Math.Sqrt(int.MaxValue))
+            {
+                throw new ArgumentException("Size is too big");
+            }
+
+            int[,] result = new int[size, size];
+
+            int offset = 0;
+            int itemCounter = 1;
+            int currentSide = size;
+
+            while (offset < (size / 2.0))
+            {
+                // top
+                for (int i = 0; i < currentSide; i++)
+                {
+                    result[offset, offset + i] = itemCounter;
+                    itemCounter++;
+                }
+
+                // right
+                for (int i = 0; i < currentSide - 2; i++)
+                {
+                    result[1 + offset + i, size - 1 - offset] = itemCounter;
+                    itemCounter++;
+                }
+
+                // bottom
+                if ((size / 2) - offset > 0)
+                {
+                    for (int i = 0; i < currentSide; i++)
+                    {
+                        result[size - 1 - offset, size - 1 - i - offset] = itemCounter;
+                        itemCounter++;
+                    }
+                }
+
+                // left
+                for (int i = 0; i < currentSide - 2; i++)
+                {
+                    result[size - 2 - offset - i, offset] = itemCounter;
+                    itemCounter++;
+                }
+
+                offset++;
+                currentSide -= 2;
+            }
+
+            return result;
         }
     }
 }
